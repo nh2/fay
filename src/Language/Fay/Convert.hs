59243b9e469ddbfb0c62000f7d2df9a77af4f990
Chris Done <chrisdone@gmail.com>
2012-10-11 11:27:23 +0200

    WIP

--- src/Language/Fay/Convert.hs
+++ src/Language/Fay/Convert.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
 {-# LANGUAGE ViewPatterns      #-}
@@ -11,20 +12,24 @@ module Language.Fay.Convert
   where
 
 import           Control.Applicative
-import           Control.Arrow
+
 import           Control.Monad
 import           Data.Aeson
 import           Data.Attoparsec.Number
 
+
 import           Data.Char
 import           Data.Data
 import           Data.Function
+import           Data.Generics.Aliases
+import           Data.Generics.Text
 import qualified Data.HashMap.Strict    as Map
-import           Data.List
+
 import           Data.Maybe
-import           Data.Ord
+
 import qualified Data.Text              as Text
 import qualified Data.Vector            as Vector
+
 import           Numeric
 import           Safe
 import qualified Text.Show.Pretty       as Show
@@ -90,58 +95,144 @@ showToFay = Show.reify >=> convert where
   slots = zipWithM keyval (map (("slot"++).show) [1::Int ..])
   keyval key val = fmap (Text.pack key,) (convert val)
 
--- | Convert a value representing a Fay value to a Haskell value.
-readFromFay :: (Data a,Read a) => Value -> Maybe a
-readFromFay value = result where
-  result = (convert >=> readMay) value
-  convert v =
-    case v of
-      Object obj -> do
-        name <- Map.lookup "instance" obj >>= getText
-        fmap parens (readRecord name obj <|> readData name obj)
-      Array array -> do
-        elems <- mapM convert (Vector.toList array)
-        return $ concat ["[",intercalate "," elems,"]"]
-      String str -> return (show str)
-      Number num -> return $ case num of
-        I integer -> show integer
-        D double -> show double
-      Bool bool -> return $ show bool
-      Null -> Nothing
+readFromFay :: Data a => Value -> Maybe a
+readFromFay value = do
+  skip
+  `ext1R` parseArray value
+  `extR` parseDouble value
+  `extR` parseInt value
+  `extR` parseBool value
+  `extR` parseString value
+  where skip = listToMaybe (map fst (gread ""))
 
-  getText i = case i of
-    String s -> return s
+-- | Parse a double.
+parseDouble :: Value -> Maybe Double
+parseDouble value = do
+  number <- parseNumber value
+  case number of
+    D n -> return n
     _ -> Nothing
 
-  readData name obj = do
-    fields <- forM assocs $ \(_,v) -> do
-      cvalue <- convert v
-      return cvalue
-    return (intercalate " " (Text.unpack name : fields))
-      where assocs = sortBy (comparing fst)
-                            (filter ((/="instance").fst) (Map.toList obj))
+-- | Parse an int.
+parseInt :: Value -> Maybe Int
+parseInt value = do
+  number <- parseNumber value
+  case number of
+    I n -> return (fromIntegral n)
+    _ -> Nothing
 
-  readRecord name (Map.toList -> assocs) = go (dataTypeConstrs typ)
-    where go (cons:conses) =
-            readConstructor name assocs cons <|> go conses
-          go [] = Nothing
+-- | Parse a number.
+parseNumber :: Value -> Maybe Number
+parseNumber value =
+  case value of
+    Number n -> return n
+    _ -> Nothing
 
-  readConstructor name assocs cons = do
-    let getField key =
-          case lookup key (map (first Text.unpack) assocs) of
-            Just v  -> return (key,v)
-            Nothing -> Nothing
-    fields <- forM (constrFields cons) $ \field -> do
-      (key,v) <- getField field
-      cvalue <- convert v
-      return (unwords [key,"=",cvalue])
-    guard $ not $ null fields
-    return (Text.unpack name ++
-            if null fields
-               then ""
-               else " {" ++ intercalate ", " fields ++ "}")
+-- | Parse a bool.
+parseBool :: Value -> Maybe Bool
+parseBool value =
+  case value of
+    Bool n -> return n
+    _ -> Nothing
 
-  typ = dataTypeOf $ resType result
-  resType :: Maybe a -> a
-  resType = undefined
-  parens x = "(" ++ x ++ ")"
+-- | Parse a string.
+parseString :: Value -> Maybe String
+parseString value =
+  case value of
+    String s -> return (Text.unpack s)
+    _ -> Nothing
+
+-- | Parse an array.
+parseArray :: Data a => Value -> Maybe [a]
+parseArray value =
+  case value of
+    Array xs -> mapM readFromFay (Vector.toList xs)
+    _ -> Nothing
+
+-- | Convert a value representing a Fay value to a Haskell value.
+-- readFromFay :: (Data a,Read a) => Value -> Maybe a
+-- readFromFay value = result where
+--   result = (convert typ >=> readMay) value
+-- --  result = (convert) value
+--   convert typ v =
+--     case v of
+--       Object obj -> do
+--         name <- Map.lookup "instance" obj >>= getText
+--         fmap parens (readRecord typ name obj <|> readData typ name obj)
+--       Array array -> do
+--         elems <- mapM (convert typ) (Vector.toList array) -- TODO
+--         return $ concat ["[",intercalate "," elems,"]"]
+
+--   getText i = case i of
+--     String s -> return s
+--     _ -> Nothing
+
+--   readData typ name obj = do
+--     fields <- forM (zip assocs) $ \(_,v) -> do
+--       cvalue <- convert typ v
+--       return cvalue
+--     return (intercalate " " (Text.unpack name : fields))
+--       where assocs = sortBy (comparing fst)
+--                             (filter ((/="instance").fst) (Map.toList obj))
+
+--   readRecord typ name (Map.toList -> assocs) = go (dataTypeConstrs typ)
+--       where go (cons:conses) = do
+--               readConstructor typ name assocs cons <|> go conses
+--             go [] = Nothing
+
+--   readConstructor typ name assocs cons = do
+--     let getField key =
+--           case lookup key (map (first Text.unpack) assocs) of
+--             Just v  -> return (key,v)
+--             Nothing -> Nothing
+--     fields <- forM (constrFields cons) $ \field -> do
+--       (key,v) <- getField field
+--       cvalue <- convert typ v
+--       return (intercalate " " [key,"=",cvalue])
+--     guard $ not $ null fields
+--     return (Text.unpack name ++
+--             if null fields
+--                then ""
+--                else " {" ++ intercalate ", " fields ++ "}")
+
+--   typ = dataTypeOf $ resType result
+--   resType :: Maybe a -> a
+--   resType = undefined
+--   parens x = "(" ++ x ++ ")"
+
+
+data Foo = Foo { unFoo :: Integer }
+    deriving (Eq, Show, Read, Typeable, Data)
+data Bar = Bar Foo
+    deriving (Eq, Show, Read, Typeable, Data)
+data Baz = Baz Foo'
+    deriving (Eq, Show, Read, Typeable, Data)
+data Foo' = Foo' Integer
+    deriving (Eq, Show, Read, Typeable, Data)
+
+
+showReadFay :: (Show a, Data a) => a -> Maybe a
+showReadFay a =
+    case showToFay a of
+      Nothing  -> Nothing
+      (Just j) -> readFromFay j
+
+-- works
+foo_test =
+    let foo = Foo 2
+    in (showReadFay foo) == Just foo
+
+-- fails, but shouldn't
+bar_test =
+    let bar = Bar (Foo 2)
+    in (showReadFay bar) == Just bar
+
+-- works
+foo'_test =
+    let foo = Foo' 2
+    in (showReadFay foo) == Just foo
+
+-- works
+baz_test =
+    let baz = Baz (Foo' 2)
+    in (showReadFay baz) == Just baz
