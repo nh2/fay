64449186bffac034d39d2da3861eb6e2a05fa18c
Chris Done <chrisdone@gmail.com>
2013-01-11 15:14:38 +0100

    Flag to output naked decls (unwrapped) (refs #203).

--- src/Language/Fay/Types.hs
+++ src/Language/Fay/Types.hs
@@ -23,6 +23,7 @@ module Language.Fay.Types
     ,configGClosure
     ,configExportBuiltins
     ,configExportRuntime
+    ,configNaked
     ,configPrettyPrint
     ,configHtmlWrapper
     ,configHtmlJSLibs
@@ -74,6 +75,7 @@ data CompileConfig = CompileConfig
   , configFlattenApps       :: Bool
   , configExportBuiltins    :: Bool
   , configExportRuntime     :: Bool
+  , configNaked             :: Bool
   , _configDirectoryIncludes :: [FilePath]
   , configPrettyPrint       :: Bool
   , configHtmlWrapper       :: Bool
@@ -92,7 +94,7 @@ data CompileConfig = CompileConfig
 instance Default CompileConfig where
   def =
     addConfigPackage "fay-base" $
-      CompileConfig False False True True [] False False [] False True Nothing True False False Nothing []
+      CompileConfig False False True True False [] False False [] False True Nothing True False False Nothing []
 
 -- Restrict these setters so elements aren't accidentally removed.
 
