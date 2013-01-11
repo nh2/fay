08455df61c4571a2d41991ff8aa097b04b757049
Chris Done <chrisdone@gmail.com>
2013-01-11 15:43:35 +0100

    Flag to exclude the serialization dispatcher (refs #203).

--- src/Language/Fay/Types.hs
+++ src/Language/Fay/Types.hs
@@ -34,6 +34,7 @@ module Language.Fay.Types
     ,configWall
     ,configPackageConf
     ,configExportStdlib
+    ,configDispatchers
   )
   ,configDirectoryIncludes
   ,addConfigDirectoryInclude
@@ -77,6 +78,7 @@ data CompileConfig = CompileConfig
   , configExportBuiltins    :: Bool
   , configExportRuntime     :: Bool
   , configExportStdlib      :: Bool
+  , configDispatchers       :: Bool
   , configNaked             :: Bool
   , _configDirectoryIncludes :: [FilePath]
   , configPrettyPrint       :: Bool
@@ -96,7 +98,7 @@ data CompileConfig = CompileConfig
 instance Default CompileConfig where
   def =
     addConfigPackage "fay-base" $
-      CompileConfig False False True True True False [] False False [] False True Nothing True False False Nothing []
+      CompileConfig False False True True True True False [] False False [] False True Nothing True False False Nothing []
 
 -- Restrict these setters so elements aren't accidentally removed.
 
